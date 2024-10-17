04cd1050d09fb1483c148dbf52bad8ac2ffed83c
ze-1234 <129536746+ze-1234@users.noreply.github.com>
2024-10-17 11:52:18 +0100

    reorganização da pasta docs

--- obj/Debug/netcoreapp8.0/DDDNetCore.AssemblyInfo.cs
+++ obj/Debug/netcoreapp8.0/DDDNetCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a15f5f177eb6ef5b36e331c15d4eede40ec84c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d530b9d46b642e8baaeaaa10520010c0fcbbd17")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
