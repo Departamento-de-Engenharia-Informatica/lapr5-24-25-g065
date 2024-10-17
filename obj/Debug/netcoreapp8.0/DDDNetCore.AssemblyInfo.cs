73d2134f9c13c40dd9549ae20776c3dc58a27070
ze-1234 <129536746+ze-1234@users.noreply.github.com>
2024-10-17 12:42:49 +0100

    atualizaçao dos docs

--- obj/Debug/netcoreapp8.0/DDDNetCore.AssemblyInfo.cs
+++ obj/Debug/netcoreapp8.0/DDDNetCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04cd1050d09fb1483c148dbf52bad8ac2ffed83c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb12f57610ebd940f736fd8499d4d60a8e335393")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
