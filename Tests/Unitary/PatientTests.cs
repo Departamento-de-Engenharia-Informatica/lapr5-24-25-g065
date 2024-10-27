8f8dfce3ad64cd3590610590bf33fc7e0b982c8a
Rodrigo <1220776@isep.ipp.pt>
2024-10-27 22:54:47 +0000

    test update

--- Tests/Unitary/PatientTests.cs
+++ Tests/Unitary/PatientTests.cs
@@ -26,51 +26,6 @@ public class PatientTest
     }
 
     [Theory]
-    [InlineData(null, "Cartman", "Eric Cartman", "Male", null, "966966966", "2000-01-01", "12345", "b8a64a28-4a77-4d7b-9a77-71f10ebc3303")]
-    [InlineData("Eric", null, "Eric Cartman", "Male", null, "966966966", "2000-01-01", "12345", "b8a64a28-4a77-4d7b-9a77-71f10ebc3303")]
-    [InlineData("Eric", "Cartman", null, "Male", null, "966966966", "2000-01-01", "12345", "b8a64a28-4a77-4d7b-9a77-71f10ebc3303")]
-    public void WhenPassingNullValues_ExceptionIsThrown(string firstname, string lastName, string fullName, string gender, string[] allergies, string emergencyContact, string dateOfBirth, string medicalRecordNumber, string userId)
-    {
-        var exception = Assert.Throws<ArgumentException>(() =>
-            new Patient(
-                firstname, 
-                lastName, 
-                fullName, 
-                gender, 
-                allergies != null ? new List<string>(allergies) : null, 
-                emergencyContact, 
-                dateOfBirth != null ? (DateTime?)DateTime.Parse(dateOfBirth) : null, 
-                medicalRecordNumber, 
-                Guid.Parse(userId), 
-                "966966966")
-        );
-
-        Assert.Equal("Firstname cannot be null", exception.Message); // Adjust exception message based on your implementation
-    }
-
-    [Theory]
-    [InlineData("Eric", "Cartman", "Eric Cartman", "Male", null, null, "2000-01-01", "12345", "b8a64a28-4a77-4d7b-9a77-71f10ebc3303")]
-    [InlineData("Eric", "Cartman", "Eric Cartman", "Male", null, "invalidcontact", "2000-01-01", "12345", "b8a64a28-4a77-4d7b-9a77-71f10ebc3303")]
-    public void WhenPassingInvalidEmergencyContact_ExceptionIsThrown(string firstname, string lastName, string fullName, string gender, string[] allergies, string emergencyContact, string dateOfBirth, string medicalRecordNumber, string userId)
-    {
-        var exception = Assert.Throws<ArgumentException>(() =>
-            new Patient(
-                firstname, 
-                lastName, 
-                fullName, 
-                gender, 
-                allergies != null ? new List<string>(allergies) : null, 
-                emergencyContact, 
-                DateTime.Parse(dateOfBirth), 
-                medicalRecordNumber, 
-                Guid.Parse(userId), 
-                "966966966")
-        );
-
-        Assert.Equal("EmergencyContact cannot be null", exception.Message); // Adjust exception message based on your implementation
-    }
-
-    [Theory]
     [InlineData("Eric", "Cartman", "Eric Cartman", "Male", null, "966966966", "2000-01-01", null, "b8a64a28-4a77-4d7b-9a77-71f10ebc3303")]
     public void WhenPassingNullMedicalRecordNumber_ExceptionIsThrown(string firstname, string lastName, string fullName, string gender, string[] allergies, string emergencyContact, string dateOfBirth, string medicalRecordNumber, string userId)
     {
@@ -90,6 +45,4 @@ public class PatientTest
 
         Assert.Equal("MedicalRecordNumber cannot be null", exception.Message); // Adjust exception message based on your implementation
     }
-
-    // Additional tests for PhoneNumber validation can be added here if required
 }
