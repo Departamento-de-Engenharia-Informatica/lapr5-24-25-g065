8634196740b0d671b744eb868c4fd12658685b79
ze-1234 <129536746+ze-1234@users.noreply.github.com>
2024-10-27 23:50:40 +0100

    operationRequestDTO

---
+++ DTOs/OperationRequest/OperationRequestDTO.cs
@@ -0,0 +1,16 @@
+﻿using DDDSample1.Domain.OperationTypes;
+using System;
+
+namespace DDDNetCore.DTOs.OperationRequest
+{
+    public class OperationRequestDTO
+    {
+        public string patientID { get; set; }
+        public string doctorID { get; set; }
+        public string operationTypeID { get; set; }
+        public string operationDateTime { get; set; }
+        public string deadline { get; set; }
+        public int priority { get; set; }
+        public OperationRequestDTO(string patientID, string doctorID, OperationTypeID, string operationDateTime, string) { }
+    }
+}
