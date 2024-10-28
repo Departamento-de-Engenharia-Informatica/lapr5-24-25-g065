4f8fda08ae2edfa4985f417223190aa336c14ecd
ze-1234 <129536746+ze-1234@users.noreply.github.com>
2024-10-28 02:45:38 +0100

    Domain OperationRequest

---
+++ Domain/OperationRequest/OperationRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DDDNetCore.Domain.OperationRequest
+{
+    public class OperationRequest{
+        public string patientID { get; set; }
+        public string doctorID { get; set; }
+        public string operationTypeID { get; set; }
+        public DateTime operationDateTime { get; set; }
+        public DateTime deadline { get; set; }
+        public int priority { get; set; }
+        public OperationRequest(string patientID, string doctorID, string operationTypeID, DateTime operationDateTime, DateTime  deadline, int priority){
+            this.patientID = patientID;
+            this.doctorID = doctorID;
+            this.operationTypeID = operationTypeID;
+            this.operationDateTime = operationDateTime;
+            this.deadline = deadline;
+            this.priority = priority;
+        }
+
+        internal void Update(string patientID, string doctorID, string operationTypeID, string operationDateTime, string deadline, int priority)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Domain/OperationRequest/OperationRequestID.cs
@@ -0,0 +1,21 @@
+﻿using DDDSample1.Domain.Shared;
+
+namespace DDDNetCore.Domain.OperationRequest
+{
+    public class OperationRequestID : EntityId
+    {
+        public OperationRequestID(object value) : base(value)
+        {
+        }
+
+        public override string AsString()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override object createFromString(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
