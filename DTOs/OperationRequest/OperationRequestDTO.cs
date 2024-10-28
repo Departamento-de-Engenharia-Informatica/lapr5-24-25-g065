03c85623352c4ef166a69bf4c285182246ccc6bd
ze-1234 <129536746+ze-1234@users.noreply.github.com>
2024-10-28 02:45:44 +0100

    Update OperationRequestDTO.cs

--- DTOs/OperationRequest/OperationRequestDTO.cs
+++ DTOs/OperationRequest/OperationRequestDTO.cs
@@ -11,7 +11,14 @@ namespace DDDNetCore.DTOs.OperationRequest
         public string operationDateTime { get; set; }
         public string deadline { get; set; }
         public int priority { get; set; }
-        public OperationRequestDTO(string patientID, string doctorID, OperationTypeID, string operationDateTime, string) { }
+        public OperationRequestDTO(string patientID, string doctorID, string operationTypeID, string operationDateTime, string deadline, int priority) {
+            this.patientID = patientID;
+            this.doctorID = doctorID;
+            this.operationTypeID = operationTypeID;
+            this.operationDateTime = operationDateTime;
+            this.deadline = deadline;
+            this.priority = priority;
+        }
     }
 }
 */
