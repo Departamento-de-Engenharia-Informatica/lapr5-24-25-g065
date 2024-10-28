dc36fc5a1198420545934dd78464aae129917c3e
ze-1234 <129536746+ze-1234@users.noreply.github.com>
2024-10-28 02:45:50 +0100

    Create IOperationRequestRepository.cs

---
+++ IRepos/IOperationRequestRepository.cs
@@ -0,0 +1,18 @@
+﻿using DDDNetCore.Domain.OperationRequest;
+using DDDNetCore.DTOs.OperationRequest;
+using DDDSample1.Domain.Patients;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DDDNetCore.IRepos
+{
+    public interface IOperationRequestRepository{
+
+        Task<IEnumerable<OperationRequest>> GetAllOperationRequest();
+        Task<OperationRequest> GetOperationRequestByPriority(int priority);
+        Task AddOperationRequest(OperationRequest operationRequest);
+        Task UpdateOperationRequest(OperationRequest operationRequest);
+        Task DeleteOperationRequestByPriority(int priority);
+        
+    }
+}
