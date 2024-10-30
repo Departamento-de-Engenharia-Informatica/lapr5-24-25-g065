b270fa23803f24946bd6369c72f0368e14ea7933
ze-1234 <129536746+ze-1234@users.noreply.github.com>
2024-10-30 11:23:24 +0100

    [#16] - Criação da classe OperationRequestID

---
+++ Domain/OperationRequestDomain/OperationRequestID.cs
@@ -0,0 +1,23 @@
+﻿using DDDSample1.Domain.Shared;
+using System;
+
+namespace DDDNetCore.Domain
+{
+    public class OperationRequestID : EntityId{
+        public OperationRequestID(object value) : base(value){
+        }
+
+        public override string AsString(){
+            Guid obj = (Guid)base.ObjValue;
+            return obj.ToString();
+        }
+
+        protected override object createFromString(string text){
+            return new Guid(text);
+        }
+        public Guid AsGuid()
+        {
+            return (Guid)base.ObjValue;
+        }
+    }
+}
