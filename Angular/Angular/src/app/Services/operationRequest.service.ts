ce15d99c518cc15760c711e28d2d0f571f2b0ac9
1170430 <59090672+1170430@users.noreply.github.com>
2024-11-24 23:48:01 +0000

    change urls

--- Angular/Angular/src/app/Services/operationRequest.service.ts
+++ Angular/Angular/src/app/Services/operationRequest.service.ts
@@ -10,14 +10,10 @@ export class OperationRequestService {
   constructor(private http: HttpClient) { }
 
   getAllOperationRequest() {
-    return this.http.get<OperationRequest[]>('https://localhost:5001/api/operationRequest');
+    return this.http.get<OperationRequest[]>('https://10.9.10.65/api/operationRequest');
   }
 
-  /*createStaff(newStaff: Staff) {
 
-    return this.http.post<Staff>('https://10.9.10.65:5001/api/Staff',newStaff);
-
-  }*/
 
   
 }
