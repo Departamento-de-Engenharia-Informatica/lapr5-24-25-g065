dc4bfca09d16c19124b7d0814fdaac2898cb152d
Alexandre Costa <1211225@isep.ipp.pt>
2024-12-02 15:49:50 +0000

    [#100] First commit of the Node.js module

--- Angular/Angular/src/app/Services/staff.service.ts
+++ Angular/Angular/src/app/Services/staff.service.ts
@@ -12,22 +12,22 @@ export class StaffService {
 
   getStaffs() {
     console.log("getting staffs");
-    return this.http.get<Staff[]>('https://10.9.10.65:5001/api/Staff');
+    return this.http.get<Staff[]>('https://localhost:5001/api/Staff');
   }
 
   createStaff(newStaff: Staff) {
 
-    return this.http.post<Staff>('https://10.9.10.65:5001/api/Staff',newStaff);
+    return this.http.post<Staff>('https://localhost:5001/api/Staff',newStaff);
 
   }
 
   updateStaff(updateStaff: any) {
-    const link = `https://10.9.10.65:5001/api/Staff/${updateStaff.Id}`;
+    const link = `https://localhost:5001/api/Staff/${updateStaff.Id}`;
     return this.http.put<Staff>(link, updateStaff);
   }
 
   deleteStaff(id: StaffId) {
-    const link = `https://10.9.10.65:5001/api/Staff/${id}/hard`;
+    const link = `https://localhost:5001/api/Staff/${id}/hard`;
     return this.http.delete<Staff>(link);
   }
 }
